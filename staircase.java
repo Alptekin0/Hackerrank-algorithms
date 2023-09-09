83e8d20f7d7812df5cf70eff0714adc71070c494
Mehmet Emin Alptekin <142679066+Alptekin0@users.noreply.github.com>
2023-09-09 17:53:18 +0300

    KARE İLE ŞEKİL
     VERİLEN "N" SAYISI BÜYÜKLÜĞÜNDE AŞAĞIDAKİ ŞEKLİ OLUŞTURMA
    		  
    		  N = 4 İSE ;
    		  
    		   #
              ##
             ###
            ####
            
            HACKERRANK LİNKİ : https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true

---
+++ staircase.java
@@ -0,0 +1,46 @@
+package HackerRank2;
+
+import java.util.Scanner;
+
+public class staircase {
+
+	public static void main(String[] args) {
+		System.out.println();
+
+		/*
+		 HACKERRANKK ALGORİTMA :
+		  
+		  VERİLEN "N" SAYISI BÜYÜKLÜĞÜNDE AŞAĞIDAKİ ŞEKLİ OLUŞTURMA
+		  
+		  N = 4 İSE ;
+		  
+		   #
+          ##
+         ###
+        ####
+        
+        HACKERRANK LİNKİ : https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true
+		  
+	*/
+
+		Scanner input = new Scanner(System.in);
+		int lenght = input.nextInt();
+		int tempLenght = lenght;
+
+		for (int counter = 0; counter < lenght; counter++) {
+			
+			for (int space = tempLenght - 1; space > 0; space--) {
+				System.out.print(" ");
+			}
+			for (int symbol = 0; symbol <= counter; symbol++) {
+				System.out.print("#");
+			}
+			System.out.println();
+			tempLenght--;
+		}
+		
+		
+
+	}
+
+}
